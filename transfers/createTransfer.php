0316f92c4c6e72f2c5dfdb000f743314164c51b5
toderesa97 <toderesa97@gmail.com>
2019-05-08 17:14:58 +0100

    getAll transfers

---
+++ transfers/createTransfer.php
@@ -0,0 +1,29 @@
+<?php
+
+include_once '../libs/Database.php';
+include_once '../libs/Checker.php';
+include_once '../libs/commonResponses/MissingFieldsOrInvalidCharactersResponse.php';
+include_once '../libs/commonResponses/NotAuthenticatedResponse.php';
+
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST");
+
+Database::createDatabaseInstance();
+
+function exec_() {
+
+}
+if (Checker::areSetAndValidFields($_POST['username'], $_POST['token'])) {
+    if (Database::isValidTokenForUser($_POST['username'], $_POST['token'])) {
+        exec_();
+    } else {
+        die(json_encode((new NotAuthenticatedResponse())->get()));
+    }
+} else {
+    die(json_encode((new MissingFieldsOrInvalidCharactersResponse())->get()));
+}
+
+// functions
+
+?>
