68e964b2c8122512cdd1ffad5ac1c219306e9ff4
Juan Antonio <juan.caballero107@alu.ulpgc.es>
2019-05-08 21:05:45 +0100

    get all nicknames of hotel and names and surnames of drivers

---
+++ transfers/getAllDrivers.php
@@ -0,0 +1,26 @@
+<?php
+
+include_once '..\libs\Database.php';
+include_once '..\libs\Checker.php';
+
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST");
+
+Database::createDatabaseInstance();
+
+function exec_()
+{
+    $retrievedData = Database::executeSQL("SELECT D.name, D.surname
+                                                    FROM drivers D, transfers T
+                                                    WHERE D.socialSecurityNumber = T.driver");
+    if ($retrievedData) {
+        $response = array();
+        foreach ($retrievedData as $row) {
+            $response[] = array('name' => $row['name'], 'surname' => $row['surname']);
+        }
+        echo json_encode($response);
+    }
+}
+
+exec_();
