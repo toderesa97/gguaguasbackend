161a1ef29013ec0e35d87db71eecb8a6d9363492
Amisadai González <wolxonmc@gmail.com>
2019-05-08 20:19:48 +0100

    getByDate move to transfer folder

--- transfers/getByDate.php
+++ transfers/getByDate.php
@@ -1,35 +1,46 @@
 <?php
-include_once '../services/Database.php';
 include '../libs/DataToJson.php';
+include_once '..\libs\Database.php';
+include_once '../libs/commonResponses/NotAuthenticatedResponse.php';
+include_once '../libs/commonResponses/MissingFieldsOrInvalidCharactersResponse.php';
+
 
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers,  Access-Control-Allow-Methods, Authorization, X-Requested-With");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Origin: *");
 
-$database = new Database();
-$db = $database->getConnection();
-$tablaMer = 'mercedes';
-$tablaMini = 'minibus';
-$tablaVtc = 'vtc';
+Database::createDatabaseInstance();
 
-$queryMer = "SELECT * FROM " . $tablaMer . " WHERE transferDate = date(?)";
-$queryMini = "SELECT * FROM " . $tablaMini . " WHERE transferDate = date(?)";
-$queryVtc = "SELECT * FROM " . $tablaVtc . " WHERE transferDate = date(?)";
-$param = array((string)$_POST['transferDate']);
+function exec_() {
 
+    $tablaMer = 'mercedes';
+    $tablaMini = 'minibus';
+    $tablaVtc = 'vtc';
 
-$dataMer = $database->query($queryMer, $param);
-$dataMini = $database->query($queryMini, $param);
-$dataVtc = $database->query($queryVtc, $param);
+    $param = array((string)$_POST['transferDate']);
 
+    $dataMer = Database::executeSQL("SELECT * FROM " . $tablaMer . " WHERE transferDate = ?", $param);
+    $dataMini = Database::executeSQL("SELECT * FROM " . $tablaMini . " WHERE transferDate = ?", $param);
+    $dataVtc = Database::executeSQL("SELECT * FROM " . $tablaVtc . " WHERE transferDate = ?", $param);
 
-$dataToJson = new DataToJson();
-$resultMer = $dataToJson->convert($dataMer);
-$resultMini = $dataToJson->convert($dataMini);
-$resultVtc = $dataToJson->convert($dataVtc);
+    $dataToJson = new DataToJson();
+    $resultMer = $dataToJson->convert($dataMer);
+    $resultMini = $dataToJson->convert($dataMini);
+    $resultVtc = $dataToJson->convert($dataVtc);
 
-$result = json_encode(array_merge(json_decode($resultMer,true),json_decode($resultMini,true),json_decode($resultVtc,true)));
+    $result = json_encode(array_merge(json_decode($resultMer,true),json_decode($resultMini,true),json_decode($resultVtc,true)));
 
-http_response_code(200);
-echo $result;
+    http_response_code(200);
+    echo $result;
+}
+
+if (Checker::areSetAndValidFields($_POST['username'], $_POST['token'])) {
+    if (Database::isValidTokenForUser($_POST['username'], $_POST['token'])) {
+        exec_();
+    } else {
+        die(json_encode((new NotAuthenticatedResponse())->get()));
+    }
+} else {
+    die(json_encode((new MissingFieldsOrInvalidCharactersResponse())->get()));
+}
